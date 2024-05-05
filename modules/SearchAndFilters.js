7a44bda6c9acbbddbfb1327a3cdd623ba3cccb5f
l0ck-git <bohdanw@ukr.net>
2024-05-05 18:05:35 +0300

    [OPT] optimization code

--- modules/SearchAndFilters.js
+++ modules/SearchAndFilters.js
@@ -1,30 +1,32 @@
-document.querySelector('#search-button').addEventListener('click', () => {
-    Find();
+const searchButton = document.querySelector('#search-button');
+const searchText = document.getElementById('text-to-find');
+const clearSearchButton = document.getElementById('clear-search');
+
+searchButton.addEventListener('click', Find);
+
+searchText.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') Find();
+});
+
+clearSearchButton.addEventListener('click', () => {
+  searchText.value = '';
+  const checkboxes = document.querySelectorAll('.searchNav input[type="checkbox"]');
+  checkboxes.forEach(checkbox => {
+    checkbox.checked = false;
+    sessionStorage.setItem(checkbox.id, 'false');
   });
+  const cards = document.querySelectorAll('.card');
+  cards.forEach(card => card.style.display = '');
+  renderData(defaultData);
+});
+
+function Find() {
+  const searchQuery = searchText.value.trim().toLowerCase();
+  const cards = document.querySelectorAll('.card');
   
-  document.getElementById('text-to-find').addEventListener('keydown', (event) => {
-    if (event.keyCode === 13) Find();
+  cards.forEach(card => {
+    const content = card.textContent || card.innerText;
+    card.style.display = content.toLowerCase().includes(searchQuery) ? '' : 'none';
   });
-  
-  document.getElementById('clear-search').addEventListener('click', () => {
-    document.getElementById('text-to-find').value = '';
-    const checkboxes = document.querySelectorAll('.searchNav input[type="checkbox"]');
-    checkboxes.forEach(checkbox => {
-      checkbox.checked = false;
-      sessionStorage.setItem(checkbox.id, 'false');
-    });
-    const cards = document.querySelectorAll('.card');
-    cards.forEach(card => card.style.display = '');
-    renderData(defaultData);
-  });
-  
-  function Find() {
-    const searchQuery = document.getElementById('text-to-find').value.trim().toLowerCase();
-    const cards = document.querySelectorAll('.card');
-    
-    cards.forEach(card => {
-      const content = card.textContent || card.innerText;
-      card.style.display = content.toLowerCase().includes(searchQuery) ? '' : 'none';
-    });
-  }
-  
+}
+
